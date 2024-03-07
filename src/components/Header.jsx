3fe94c7c6b2f24c5866fdd34cbc917d3c8d3cfba
Ayenco <joluwabunmi75@gmail.com>
2024-03-07 16:25:49 +0100

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Header = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const pathMathRoute=(route)=>{
+        if(route === location.pathname){
+            return true;
+        }
+    }
+  return (
+    <div className='b-white border-b shadow-sm max-w-6xl mx-auto sticky top-0 z-50 py-3'>
+      <header className='flex justify-between items-center px-8'>
+        <div>
+            <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" 
+            alt="logo" className='h-5 cursor-pointer' onClick={()=>navigate("/")} />
+        </div>
+        <div>
+            <ul className='flex space-x-10'>
+                <li className={`text-sm border-b-[3px] text-gray-600 py-3 border-transparent cursor-pointer ${pathMathRoute("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
+                <li  className={`text-sm border-b-[3px] text-gray-600 py-3 border-transparent cursor-pointer ${pathMathRoute("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate("/offers")}>Offers</li>
+                <li  className={`text-sm border-b-[3px] text-gray-600 py-3 border-transparent cursor-pointer ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Sign in</li>
+            </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
