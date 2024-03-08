737870b9396f863e35ea7faf6a2139b6483747f0
Ayenco <joluwabunmi75@gmail.com>
2024-03-08 09:09:11 +0100

    complete OAuth functionalities

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,10 +1,36 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import React from 'react';
 import { FcGoogle } from "react-icons/fc";
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 
-const OAuth = () => {
+const OAuth =() => {
+  const navigate = useNavigate()
+  const onGoogleClick=async()=>{
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth,provider);
+      const user = result.user;
+      const docRef = doc(db, "users", user.udi);
+      const snapShot = getDoc(docRef);
+      if(!snapShot.exists){
+        await setDoc(docRef,{
+          name:user.displayName,
+          email:user.email,
+          timestamp:serverTimestamp()
+        })
+      }
+      navigate("/")
+    } catch (error) {
+      toast.error("Could not authorize with Google")
+    }
+  }
   return (
-      <button type="" className='flex justify-center items-center text-xl font-semibold
+      <button type="button" onClick={onGoogleClick} className='flex justify-center items-center text-xl font-semibold
        px-7 py-3 bg-red-600
        w-full text-white shadow-md hover:bg-red-700 active:bg-red-800 hover:shadow-lg active:shadow-lg 
        rounded-xl cursor-pointer'>
